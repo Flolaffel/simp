65619514395066e3f146006ecddbcae49e799199
Florian Rueckert <florian.rueckert@mb.tu-chemnitz.de>
2024-02-16 10:02:49 +0100

    Volume response class, computes volume and volume sensitivity

---
+++ include/userobjects/VolumeResponse.h
@@ -0,0 +1,56 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "TopologyOptimizationDesignResponse.h"
+
+/**
+ * Element user object that copmutes the absolute value and sensitivity for a volume response in
+ * Solid Isotropic Material Penalization (SIMP) optimization.
+ */
+class VolumeResponse : public TopologyOptimizationDesignResponse
+{
+public:
+  static InputParameters validParams();
+
+  VolumeResponse(const InputParameters & parameters);
+
+  virtual void initialize() override;
+  virtual void execute() override;
+  virtual void finalize() override{};
+  virtual void threadJoin(const UserObject &) override{};
+
+  virtual void gatherElementData() override;
+
+  /**
+   * Computes g = sum(x)/V_lim - 1
+   */
+  virtual void computeValue() override;
+
+  /**
+   * Computes dg/dx = 1/V_lim
+   */
+  virtual void computeSensitivity() override;
+
+private:
+  struct ElementData
+  {
+    Real physical_density;
+    Real new_volume_sensitivity;
+    ElementData() = default;
+    ElementData(Real dens, Real new_dv) : physical_density(dens), new_volume_sensitivity(new_dv) {}
+  };
+
+  /// Data structure to hold elemental values
+  std::map<dof_id_type, ElementData> _elem_data_map;
+
+  /// Element number
+  unsigned int _n_el;
+};
---
+++ src/userobjects/VolumeResponse.C
@@ -0,0 +1,99 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "VolumeResponse.h"
+#include "MooseVariableScalar.h"
+#include <algorithm>
+
+// #include "MooseVariableBase.h"
+
+registerMooseObject("OptimizationApp", VolumeResponse);
+
+InputParameters
+VolumeResponse::validParams()
+{
+  InputParameters params = TopologyOptimizationDesignResponse::validParams();
+  params.addClassDescription("Computes the volume sensitivities for volume constrained topology "
+                             "optimization (2D ONLY). RELATIVE LIMIT");
+  return params;
+}
+
+VolumeResponse::VolumeResponse(const InputParameters & parameters)
+  : TopologyOptimizationDesignResponse(parameters)
+{
+}
+
+void
+VolumeResponse::initialize()
+{
+  gatherElementData();
+  _n_el = _elem_data_map.size();
+  computeValue();
+  computeSensitivity();
+}
+
+void
+VolumeResponse::execute()
+{
+  // Grab the element data for each id
+  auto elem_data_iter = _elem_data_map.find(_current_elem->id());
+
+  // Check if the element data is not null
+  if (elem_data_iter != _elem_data_map.end())
+  {
+    ElementData & elem_data = elem_data_iter->second;
+    dynamic_cast<MooseVariableFE<Real> *>(_sensitivity)
+        ->setNodalValue(elem_data.new_volume_sensitivity);
+  }
+  else
+  {
+    mooseError("Element data not found for the current element id.");
+  }
+}
+
+void
+VolumeResponse::gatherElementData()
+{
+  _elem_data_map.clear();
+
+  for (const auto & sub_id : blockIDs())
+    for (const auto & elem : _mesh.getMesh().active_local_subdomain_elements_ptr_range(sub_id))
+    {
+      dof_id_type elem_id = elem->id();
+
+      ElementData data = ElementData(
+          dynamic_cast<const MooseVariableFE<Real> *>(_physical_density)->getElementalValue(elem),
+          0);
+      _elem_data_map[elem_id] = data;
+    }
+}
+
+void
+VolumeResponse::computeValue()
+{
+  Real constraint_value = 0;
+  for (auto && [id, elem_data] : _elem_data_map)
+  {
+    constraint_value += elem_data.physical_density;
+  }
+  constraint_value /= _limit * _n_el;
+  constraint_value -= 1;
+  _value->setValues(constraint_value);
+  _value->insert(_value->sys().solution());
+  _value->sys().solution().close();
+}
+
+void
+VolumeResponse::computeSensitivity()
+{
+  for (auto && [id, elem_data] : _elem_data_map)
+  {
+    elem_data.new_volume_sensitivity = 1 / (_limit * _n_el);
+  }
+}
