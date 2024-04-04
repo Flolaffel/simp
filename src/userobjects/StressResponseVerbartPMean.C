72cb87b43ff6a0d36fcd7ed295d6660421fd997f
Florian Rueckert <florian.rueckert@mb.tu-chemnitz.de>
2024-04-04 15:32:04 +0200

    Stress constraint class without relaxation and P-mean aggregation

---
+++ src/userobjects/StressResponseVerbartPMean.C
@@ -0,0 +1,118 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "StressResponseVerbartPMean.h"
+#include "MooseVariableScalar.h"
+
+registerMooseObject("OptimizationApp", StressResponseVerbartPMean);
+
+InputParameters
+StressResponseVerbartPMean::validParams()
+{
+  InputParameters params = StressResponseBase::validParams();
+  params.addClassDescription("Computes the stress sensitivities for stress constrained topology "
+                             "optimization using qp-relaxation and P-norm aggregation (2D ONLY).");
+  return params;
+}
+
+StressResponseVerbartPMean::StressResponseVerbartPMean(const InputParameters & parameters)
+  : StressResponseBase(parameters)
+{
+}
+
+void
+StressResponseVerbartPMean::computeStress()
+{
+  _stress.resize(_n_el, 3);
+  _vonmises.resize(_n_el);
+  for (auto && [id, elem_data] : _elem_data_map)
+  {
+    RealEigenVector vector = _E * getBMat(0, 0).transpose() * elem_data.u_el;
+    _stress.row(id) << vector.transpose();
+    _vonmises(id) = std::sqrt(std::pow(vector(0), 2) + std::pow(vector(1), 2) -
+                              vector(0) * vector(1) + 3 * std::pow(vector(2), 2));
+  }
+}
+
+void
+StressResponseVerbartPMean::computeValue()
+{
+  Real PM = 0;
+  for (auto && [id, elem_data] : _elem_data_map)
+  {
+    if (_is_objective)
+      mooseError("Verbart P-mean doesn't support objective.");
+    if (_is_constraint)
+      PM += std::pow(elem_data.physical_density * (_vonmises(id) / _limit - 1) + 1, _P);
+  }
+  PM = std::pow(1.0 / _n_el * PM, 1.0 / _P) - 1;
+  _value->reinit();
+  _value->setValues(PM);
+  _value->insert(_value->sys().solution());
+  _value->sys().solution().close();
+}
+
+void
+StressResponseVerbartPMean::computeSensitivity()
+{
+  /// dsigma^PM/drho and dsigma^PM/dsigma_VMi
+  RealEigenVector dPMdx(_n_el), dPMdVM(_n_el);
+  Real sum = 0;
+  for (auto && [id, elem_data] : _elem_data_map)
+  {
+    sum += std::pow(elem_data.physical_density * (_vonmises(id) / _limit - 1) + 1, _P);
+  }
+  for (auto && [id, elem_data] : _elem_data_map)
+  {
+    dPMdx(id) = std::pow(1.0 / _n_el, 1.0 / _P) * 1.0 / _P * std::pow(sum, 1.0 / _P - 1) * _P *
+                std::pow(elem_data.physical_density * (_vonmises(id) / _limit - 1) + 1, _P - 1) *
+                (_vonmises(id) / _limit - 1);
+    dPMdVM(id) = std::pow(1.0 / _n_el, 1.0 / _P) * 1.0 / _P * std::pow(sum, 1.0 / _P - 1) * _P *
+                 std::pow(elem_data.physical_density * (_vonmises(id) / _limit - 1) + 1, _P - 1) *
+                 elem_data.physical_density / _limit;
+  }
+
+  /// dsigma_VMi/dsigma_i
+  std::vector<RealEigenVector> dVMdS(_n_el, RealEigenVector(3));
+  for (auto && [id, elem_data] : _elem_data_map)
+  {
+    dVMdS[id](0) = 0.5 / _vonmises(id) * (2 * _stress(id, 0) - _stress(id, 1));
+    dVMdS[id](1) = 0.5 / _vonmises(id) * (2 * _stress(id, 1) - _stress(id, 0));
+    dVMdS[id](2) = 3 / _vonmises(id) * _stress(id, 2);
+  }
+
+  /// vector gamma and gamma_red
+  std::vector<Real> gamma(_n_dofs), gamma_red(_n_dofs);
+  for (auto && [id, elem_data] : _elem_data_map)
+  {
+    RealEigenVector vector = getBMat(0, 0) * _E * dVMdS[id];
+    int x = 0;
+    for (auto & dof : _elem_to_dof_map[id])
+    {
+      gamma[dof] += dPMdVM(id) * vector(x);
+      bool dof_is_free =
+          std::find(std::begin(_free_dofs), std::end(_free_dofs), dof) != std::end(_free_dofs);
+      if (dof_is_free)
+        gamma_red[dof] += dPMdVM(id) * vector(x);
+      x++;
+    }
+  }
+
+  /// vector lambda
+  RealEigenVector lambda;
+  lambda = getLambda(gamma_red);
+
+  /// final sensitivity
+  RealEigenVector T1 = dPMdx;
+  RealEigenVector T2 = getT2(lambda);
+  for (auto && [id, elem_data] : _elem_data_map)
+  {
+    elem_data.stress_sensitivity = T1(id) + T2(id);
+  }
+}
