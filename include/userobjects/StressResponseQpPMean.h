90d07cfeb1143d64c5e54f07d231673ff1e533fb
Florian Rueckert <florian.rueckert@mb.tu-chemnitz.de>
2024-04-04 15:27:59 +0200

    Stress constraint class with qp relaxation and P-mean aggregation

---
+++ include/userobjects/StressResponseQpPMean.h
@@ -0,0 +1,39 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "StressResponseBase.h"
+
+/**
+ * Element user object that copmutes the sensitivity need for a stress constrained Solid Isotropic
+ * Material Penalization (SIMP) optimization.
+ */
+class StressResponseQpPMean : public StressResponseBase
+{
+public:
+  static InputParameters validParams();
+
+  StressResponseQpPMean(const InputParameters & parameters);
+
+  virtual void finalize() override{};
+  virtual void threadJoin(const UserObject &) override{};
+
+  virtual void computeStress() override;
+  virtual void computeValue() override;
+  virtual void computeSensitivity() override;
+
+protected:
+  /// Stress penalization Parameter
+  const Real _q;
+  /// Voigt stress vector for every Element
+  RealEigenMatrix _stress;
+  /// Elemental Von Mises stress vector
+  RealEigenVector _vonmises;
+};
