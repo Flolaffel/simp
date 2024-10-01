e3bfc8e5578d26aef300ffeee34febda335a86f9
Florian Rueckert <florian.rueckert@mb.tu-chemnitz.de>
2024-10-01 12:16:38 +0200

    Script for extracting elemental variables from exodus files

---
+++ scripts/extract_elemental.py
@@ -0,0 +1,46 @@
+import itertools
+import netCDF4 as ex
+import csv
+import numpy as np
+import os
+
+# open file
+file = ex.Dataset(
+    "problems/shear_PBC_IC/simple/stress_cons/simple_shear_u_PBC_50x50_V50_QpPN0-06_m0-01_P100.e"
+)
+
+# get names of element variables
+encodedElementVarNames = file.variables["name_elem_var"]
+elementVarNames = []
+for encName in encodedElementVarNames:
+    name = ""
+    for char in encName:
+        if not isinstance(char, np.ma.core.MaskedConstant):
+            name += char.decode("UTF-8")
+    elementVarNames.append(name)
+# print(elementVarNames)
+
+
+# get values of element variables
+checkElemVars = [
+    "interpolated_micro_vonmises_stress",
+]
+
+elemVarIndex = []
+elemVars = []
+for var in checkElemVars:
+    var = var[:32]
+    index = elementVarNames.index(var) + 1
+    # get values of desired variable
+    timeStep = 500
+    elemVars.append(
+        file.variables["vals_elem_var" + str(index) + "eb1"][timeStep].tolist()
+    )
+
+file.close()
+
+script_path = os.path.dirname(os.path.realpath(__file__))
+
+with open(script_path + "/extract_elemental.csv", "w") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(zip(*elemVars))
